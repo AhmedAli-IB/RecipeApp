49f72824d4de106eba1e5009a87356fb3d683dbd
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-03-29 00:17:03 +0200

    - Add recipe details view controller

---
+++ RecipeApp/Presentation/ViewRelated/RecipeDetailsScene/View/RecipeDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeDetailsViewController.swift
+//  RecipeApp
+//
+//  Created by Ahmed Ali on 29/03/2021.
+//
+
+import UIKit
+
+class RecipeDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
