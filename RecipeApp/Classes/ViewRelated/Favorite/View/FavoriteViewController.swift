eb173dfa27a0e865e9898c513a00114a89595936
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-03-28 14:06:04 +0200

    Add favorite view

---
+++ RecipeApp/Classes/ViewRelated/Favorite/View/FavoriteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteViewController.swift
+//  RecipeApp
+//
+//  Created by Ahmed Ali on 27/03/2021.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
