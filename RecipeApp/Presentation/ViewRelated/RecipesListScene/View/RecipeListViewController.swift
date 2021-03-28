f64701587b4e6dad56915d747c046b2a8c723db6
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-03-29 00:11:56 +0200

    Update recipe list controller and view model

--- RecipeApp/Presentation/ViewRelated/RecipesListScene/View/RecipeListViewController.swift
+++ RecipeApp/Presentation/ViewRelated/RecipesListScene/View/RecipeListViewController.swift
@@ -9,15 +9,100 @@ import UIKit
 
 class RecipeListViewController: BaseViewController {
     
+    // MARK: - IBOutlets
+    //
+    @IBOutlet private weak var tableView: UITableView!
+    
     // MARK: - Properties
     //
     let viewModel = RecipeListViewModel()
+    
     // MARK: - Lifecycle
     //
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureAppearance()
+        viewModel.viewDidLoad()
+    }
+}
+// MARK: - UITableViewDelegate, UITableViewDataSource
+//
+extension RecipeListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeue(RecipeCell.self)
+        cell.viewModel = viewModel.getCurrentObject(for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        Constants.rowHeight
+    }
+}
+
+// MARK: - Configuration
+//
+private extension RecipeListViewController {
+    
+    /// Configure appearance
+    ///
+    func configureAppearance() {
+        configureView()
+        configureViewModel()
+        registerCells()
+        configureTableView()
+    }
+    /// Configure view
+    ///
+    func configureView() {
+        title = Strings.title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let attribute  = [NSAttributedString.Key.foregroundColor: Asset.ColorPalette.primaryColor.color]
+        self.navigationController?.navigationBar.titleTextAttributes = attribute
+        self.navigationController?.navigationBar.largeTitleTextAttributes = attribute
+    }
+    /// Configure table view
+    ///
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    /// Configure view model
+    ///
+    func configureViewModel() {
         bindErrorState(to: viewModel)
         bindLoadingState(to: viewModel)
-        viewModel.loadRecipes()
+        viewModel.onReloadNeeded = { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+    }
+    /// Register cells
+    ///
+    func registerCells() {
+        tableView.registerCellNib(RecipeCell.self)
+    }
+}
+// MARK: - Constants
+//
+private extension RecipeListViewController {
+    
+     enum Constants {
+        static let rowHeight = CGFloat(220)
+    }
+}
+
+// MARK: - Strings
+//
+private extension RecipeListViewController {
+    
+    enum Strings {
+        static let title = "Recipes"
     }
 }
--- RecipeApp/Presentation/ViewRelated/RecipesListScene/ViewModel/RecipeListViewModel.swift
+++ RecipeApp/Presentation/ViewRelated/RecipesListScene/ViewModel/RecipeListViewModel.swift
@@ -14,6 +14,11 @@ class  RecipeListViewModel: BaseViewModel {
     // MARK: - Properties
     //
     let recipeUseCase: RecipeUseCaseType
+    private var recipes: [RecipeMainModel] = []
+    
+    // MARK: - Callbacks
+    //
+    var onReloadNeeded: (() -> Void)?
     
     // MARK: - Init
     //
@@ -23,6 +28,25 @@ class  RecipeListViewModel: BaseViewModel {
     
     // MARK: - Handlers
     //
+    func viewDidLoad() {
+        loadRecipes()
+    }
+    /// number of rows
+    ///
+    var numberOfRows: Int {
+        return recipes.count
+    }
+    /// Get current object based on index path
+    ///
+    func getCurrentObject(for indexPath: IndexPath) -> RecipeMainModel {
+        return recipes[indexPath.row]
+    }
+}
+
+// MARK: - Private Handlers
+//
+extension RecipeListViewModel {
+    
     func loadRecipes() {
         state.send(.loading)
         recipeUseCase.execute { [weak self] result in
@@ -30,7 +54,8 @@ class  RecipeListViewModel: BaseViewModel {
             switch result {
             case .success(let recipes):
                 self.state.send(.success)
-                print("recipes==\(recipes)")
+                self.recipes = recipes
+                self.onReloadNeeded?()
             case .failure(let error):
                 self.state.send(.failure(error.localizedDescription))
             }
