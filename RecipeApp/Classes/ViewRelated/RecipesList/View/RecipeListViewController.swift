c4b48238776d72f2ed286dd408ce16ad3e05ffe5
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-03-28 14:06:32 +0200

    - Add recipe list view

---
+++ RecipeApp/Classes/ViewRelated/RecipesList/View/RecipeListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecipeListViewController.swift
+//  RecipeApp
+//
+//  Created by Ahmed Ali on 27/03/2021.
+//
+
+import UIKit
+
+class RecipeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
