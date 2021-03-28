286ec1f1b15ac96ab1d43b9a10569d9035f49cf4
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-03-28 16:55:33 +0200

    Update app structure Add user entity

---
+++ RecipeApp/Domain/Entities/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  RecipeApp
+//
+//  Created by Ahmed Ali on 28/03/2021.
+//
+
+import Foundation
+// MARK: - User
+/// `User` entity contain user name and user id.
+///
+struct User {
+    let name: String
+    let userID: String
+}
---
+++ RecipeApp/Presentation/ViewRelated/LoginScene/ViewModel/LoginViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewModel.swift
+//  RecipeApp
+//
+//  Created by Ahmed Ali on 28/03/2021.
+//
+
+import Foundation
+
